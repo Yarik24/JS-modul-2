4d4e09acee3df2652791e5eac89eb15c41abe65b
Yarik24 <jarslavkblv24@gmail.com>
2021-06-21 11:53:31 +0300

    дз2

--- homeWork1/shopTest.js
+++ homeWork1/shopTest.js
@@ -1,11 +1,12 @@
-class ProductList{
-    constructor(container='.products'){
+class ProductsList {
+    constructor(container = '.products'){
         this.container = container;
         this.goods = [];
+        this.allProducts = [];
         this._fetchProducts();
-        this.render();
     }
-    _fetchProducts(){
+    
+    _fetchProducts() {
         this.goods = [
             {id: 1, title: 'Notebook', price: 2000},
             {id: 2, title: 'Mouse', price: 20},
@@ -13,32 +14,71 @@ class ProductList{
             {id: 4, title: 'Gamepad', price: 50},
         ];
     }
-    
-    render(){
+    render() {
         const block = document.querySelector(this.container);
         for(let product of this.goods){
-            const item = new ProductItem(product);
-             block.insertAdjacentHTML("beforeend",item.render());
-
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend',productObj.render())
         }
     }
+    getSum() {
+//         let s = 0;
+//         this.goods.forEach(item=>{
+//             s += item.price;
+//         })
+        
+//        let sum = 0;
+//        for(let product of this.goods){
+//            sum += product.price;
+//        }
+        //reduce используется для последовательной обработки каждого элемента массива с сохранением промежуточного результата.
+//        let res = this.allProducts.reduce((sum, item) => sum + item.price,0);
+//        alert(res);
+    }    
 }
 
-class ProductItem{
-    constructor(product,img='https://via.placeholder.com/200x150'){
-        this.title = product.title;
-        this.id = product.id;
-        this.price = product.price;
-        this.img = img;
-    }
-    render(){
-           return `<div class="product-item">
-                <img src="${this.img}">
+
+class ProductItem {
+	constructor(product, img = 'https://via.placeholder.com/200x150'){
+		this.title = product.title;
+		this.price = product.price;
+		this.id = product.id;
+		this.img = img;
+		
+	}
+	
+	render() {
+		 return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
                 <h3>${this.title}</h3>
                 <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
+	}
+}
+
+let list = new ProductsList();
+list.render();
+list.getSum();
+
+class Basket {
+    addGoods() {
+
+    }
+    removeGoods() {
+
+    }
+    changeGoods() {
+
+    }
+    
+    render(){
+        
     }
 }
 
-let list = new ProductList();
+class ElemBasket {
+    render(){}
+
+}
